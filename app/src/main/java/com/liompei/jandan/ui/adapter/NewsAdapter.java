cf605e0ca98f2672fc88401344f1d2656564785e
liompei <blm1137694912@gmail.com>
2017-11-08 22:14:35 +0800

    优化新鲜事item

--- app/src/main/java/com/liompei/jandan/ui/adapter/NewsAdapter.java
+++ app/src/main/java/com/liompei/jandan/ui/adapter/NewsAdapter.java
@@ -3,6 +3,10 @@ package com.liompei.jandan.ui.adapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.liompei.jandan.R;
 
 /**
  * Created by Liompei
@@ -30,8 +34,18 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.MyHolder>{
     }
 
     class MyHolder extends RecyclerView.ViewHolder {
+        ImageView iv_img;
+        TextView tv_title;
+        TextView tv_author;
+        TextView tv_time;
+        TextView tv_comment;
         public MyHolder(View itemView) {
             super(itemView);
+            iv_img=itemView.findViewById(R.id.iv_img);
+            tv_title=itemView.findViewById(R.id.tv_title);
+            tv_author=itemView.findViewById(R.id.tv_author);
+            tv_time=itemView.findViewById(R.id.tv_time);
+            tv_comment=itemView.findViewById(R.id.tv_comment);
         }
     }
 }
